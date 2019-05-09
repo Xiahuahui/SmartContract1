80a2aa955a6afbf43ebcbe02486ababa8b410f25
xiahuahui <1204289544@qq.com>
2019-05-09 20:51:05 +0800

    tijiao

---
+++ SmartContract1-master/code/f3452a9e.sol
@@ -0,0 +1,244 @@
+//BCMETH means Blockchain Match Ethereum 
+pragma solidity ^0.4.24;
+
+contract BCMETH {
+    String currentStatus;
+    constructor () public {
+        currentStatus="[2, 1, 1, 1, 1, 1]";
+    }
+
+    function (Sat0)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Vio0)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Sat1)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Vio1)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp1)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Sat2)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Vio2)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp2, Sat5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp2, Vio5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp2)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp3, Sat4)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp3, Vio4)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Sat3)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Vio3)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp3)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp3)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp3)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp4, Sat5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp4, Vio5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Sat4, Exp5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Vio4, Exp5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp4)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp4)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function (Exp4, Exp5)(String actionStr) public returns(bool){
+        if(currentStatus=="[2, 1, 1, 1, 1, 1]" && action=="(Sat0)"){
+            currentStatus="[3, 2, 1, 1, 1, 1]";
+            return true;
+        }
+        else
+            return false;
+    }
+
+}
